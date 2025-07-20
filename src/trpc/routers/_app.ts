41e87a51b3876c9cb28ef54a13c90642e669284d
Abhishek Pal <abhishekpal1413@gmail.com>
2025-07-20 11:34:21 +0530

    feat: add project management functionality with CRUD operations and integrate projectId in messages

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,9 @@
+import { ProjectsRouter } from "@/modules/projects/server/procedures";
 import { createTRPCRouter } from "../init";
 import { messageRouter } from "@/modules/messages/server/procedures";
 export const appRouter = createTRPCRouter({
   messages: messageRouter,
+  projects: ProjectsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
